71ffb30d6cb6bc9de59a01e5cf33467c9d94e4b1
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-09-21 08:40:06 +0530

    Cleaned up the code for Dock Component
    fixes #8
    addresses the second point raised in #11

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -35,217 +35,169 @@ const Dock = () => {
   const setSaveFSM = useSetAtom(saveFSMAtom);
   const saveFSM = useAtomValue(saveFSMAtom);
 
+  // Dock Items
+  // Each item has a name, icon, and onclick function
+  // The condition array is used to determine the background color of
+  // that item's button when it is not active and active respectively
+  const DockItems = [
+    {
+      name: "Displace",
+      condition: [currentState != "grab", currentState == "grab"],
+      icon: (
+        <Move3d
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => {
+        currentState == "grab"
+          ? setCurrentState("nil")
+          : setCurrentState("grab");
+      },
+    },
+    {
+      name: "Move",
+      condition: [currentState != "select", currentState == "select"],
+      icon: (
+        <Move
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => {
+        currentState == "select"
+          ? setCurrentState("nil")
+          : setCurrentState("select");
+      },
+    },
+    {
+      name: "Add",
+      condition: [currentState != "create", currentState == "create"],
+      icon: (
+        <PlusCircleIcon
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => {
+        currentState == "create"
+          ? setCurrentState("nil")
+          : setCurrentState("create");
+      },
+    },
+    {
+      name: "Delete",
+      condition: [currentState != "delete", currentState == "delete"],
+      icon: (
+        <MinusCircleIcon
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => {
+        currentState == "delete"
+          ? setCurrentState("nil")
+          : setCurrentState("delete");
+      },
+    },
+    {
+      name: "Controls",
+      condition: [currentState != "settings", currentState == "settings"],
+      icon: (
+        <Settings
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+
+      onclick: () => {
+        if (currSelected != "nil") {
+          currentState == "settings"
+            ? setCurrentState("nil")
+            : setCurrentState("settings");
+        } else {
+          setAlertMsg("You must select a State to view its Settings!");
+          setTimeout(() => setAlertMsg("nil"), 3000);
+        }
+      },
+    },
+    {
+      name: "Connect",
+      condition: [currentState != "connect", currentState == "connect"],
+      icon: (
+        <Cable
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => {
+        if (currentState == "connect") setCurrentState("nil");
+        else {
+          setCurrentState("connect");
+          setTransitionTracker(undefined);
+        }
+      },
+    },
+
+    {
+      name: "Save FSM",
+      condition: [!saveFSM, saveFSM],
+      icon: (
+        <HardDriveDownload
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => setSaveFSM(!saveFSM),
+    },
+
+    {
+      name: "Welcome",
+      condition: [currentState != "welcome", currentState == "welcome"],
+      icon: (
+        <BookMarked
+          size={DockIconSize}
+          color={DockIconColor}
+          className="pointer-events-none"
+        />
+      ),
+      onclick: () => {
+        currentState == "welcome"
+          ? setCurrentState("nil")
+          : setCurrentState("welcome");
+      },
+    },
+  ];
+
   return (
     <div className="absolute bottom-5 w-screen h-15 flex justify-center items-center select-none max-lg:hidden">
       <div className="w-fit px-2 h-15 z-10 bg-secondary-bg rounded-2xl border border-border-bg flex justify-center items-center gap-5 shadow-[0px_0px_40px_0px_rgba(0,0,0,0.5)]">
         {/* Dock Items */}
-
-        {/* Grab/Move Editor */}
-        <div
-          onClick={() =>
-            currentState == "grab"
-              ? setCurrentState("nil")
-              : setCurrentState("grab")
-          }
-          className={clsx(
-            "group p-2 border flex gap-2 border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "grab",
-              "bg-blue-500": currentState == "grab",
-            }
-          )}
-        >
-          <Move3d
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-          <p className="text-white font-github font-semibold text-balance">
-            Displace
-          </p>
-        </div>
-
-        {/* Select/Move Nodes */}
-        <div
-          onClick={() =>
-            currentState == "select"
-              ? setCurrentState("nil")
-              : setCurrentState("select")
-          }
-          className={clsx(
-            "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "select",
-              "bg-blue-500": currentState == "select",
-            }
-          )}
-        >
-          <Move
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-
-          <p className="text-white font-github font-semibold text-balance">
-            Move
-          </p>
-        </div>
-
-        {/* Create Node */}
-        <div
-          onClick={() =>
-            currentState == "create"
-              ? setCurrentState("nil")
-              : setCurrentState("create")
-          }
-          className={clsx(
-            "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "create",
-              "bg-blue-500": currentState == "create",
-            }
-          )}
-        >
-          <PlusCircleIcon
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-          <p className="text-white font-github font-semibold text-balance">
-            Add
-          </p>
-        </div>
-
-        {/* Delete Node */}
-        <div
-          onClick={() =>
-            currentState == "delete"
-              ? setCurrentState("nil")
-              : setCurrentState("delete")
-          }
-          className={clsx(
-            "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "delete",
-              "bg-blue-500": currentState == "delete",
-            }
-          )}
-        >
-          <MinusCircleIcon
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-          <p className="text-white font-github font-semibold text-balance">
-            Delete
-          </p>
-        </div>
-
-        {/* Settings of a Node */}
-        <div
-          onClick={() => {
-            if (currSelected != "nil") {
-              currentState == "settings"
-                ? setCurrentState("nil")
-                : setCurrentState("settings");
-            } else {
-              setAlertMsg("You must select a State to view its Settings!");
-              setTimeout(() => setAlertMsg("nil"), 3000);
-            }
-          }}
-          className={clsx(
-            "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "settings",
-              "bg-blue-500": currentState == "settings",
-            }
-          )}
-        >
-          <Settings
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-          <p className="text-white font-github font-semibold text-balance">
-            Controls
-          </p>
-        </div>
-
-        {/* Connect Nodes */}
-        <div
-          onClick={() => {
-            if (currentState == "connect") setCurrentState("nil");
-            else {
-              setCurrentState("connect");
-              setTransitionTracker(undefined);
-            }
-          }}
-          className={clsx(
-            "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "connect",
-              "bg-blue-500": currentState == "connect",
-            }
-          )}
-        >
-          <Cable
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-
-          <p className="text-white font-github font-semibold text-balance">
-            Connect
-          </p>
-        </div>
-
-        {/* Save/Download FSM */}
-        <div
-          onClick={() => setSaveFSM(!saveFSM)}
-          className={clsx(
-            "group p-2 border flex gap-2 border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": !saveFSM,
-              "bg-blue-500": saveFSM,
-            }
-          )}
-        >
-          <HardDriveDownload
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-          <p className="text-white font-github font-semibold text-balance">
-            Save FSM
-          </p>
-        </div>
-
-        {/* Help/Welcome FSM */}
-        <div
-          onClick={() =>
-            currentState == "welcome"
-              ? setCurrentState("nil")
-              : setCurrentState("welcome")
-          }
-          className={clsx(
-            "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-130 hover:-translate-y-5 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
-            {
-              "bg-secondary-bg": currentState != "welcome",
-              "bg-blue-500": currentState == "welcome",
-            }
-          )}
-        >
-          <BookMarked
-            size={DockIconSize}
-            color={DockIconColor}
-            className="pointer-events-none"
-          />
-          <p className="text-white font-github font-semibold text-balance">
-            Welcome
-          </p>
-        </div>
-
-        {/* Dock Items */}
+        {DockItems.map((item) => (
+          <div
+            key={item.name}
+            onClick={item.onclick}
+            className={clsx(
+              "flex gap-2 p-2 border border-border-bg rounded-xl hover:scale-110 hover:-translate-y-3 active:scale-100 cursor-pointer transition-all ease-in-out duration-300",
+              {
+                "bg-secondary-bg": item.condition[0],
+                "bg-blue-500": item.condition[1],
+              }
+            )}
+          >
+            {item.icon}
+            <p className="text-white font-github font-semibold text-balance">
+              {item.name}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -42,7 +42,7 @@ export default function Welcome() {
       {/* Settings Window */}
       <div
         className={clsx(
-          "flex flex-col justify-center items-center py-8 px-5 gap-5 w-150 h-fit bg-primary-bg border border-border-bg rounded-4xl shadow-[0px_0px_100px_0px_#00000080] transition-all ease-in-out duration-500",
+          "flex flex-col justify-center items-center py-8 px-5 gap-5 w-150 h-170 bg-primary-bg border border-border-bg rounded-4xl shadow-[0px_0px_100px_0px_#00000080] transition-all ease-in-out duration-500",
           {
             "hidden pointer-events-none opacity-0 scale-0":
               currentEditorState != "welcome",
