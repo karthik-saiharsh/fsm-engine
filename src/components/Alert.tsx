9c8cac521b5fdbc127103d8a58c085d590c9aed4
Revanth Singothu <revanthsingothu@gmail.com>
2025-09-21 09:06:06 +0000

    ci: added husky

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,25 +3,25 @@ import { useAtomValue } from "jotai";
 import clsx from "clsx";
 
 const Alert = (props: { message: string }) => {
-  const alertMsg = useAtomValue(alert);
+	const alertMsg = useAtomValue(alert);
 
-  return (
-    <div
-      className={clsx(
-        "select-none w-full h-20 absolute z-50 mx-auto flex justify-center items-center transition-all ease-in-out duration-300",
-        {
-          "-top-50": alertMsg == "nil",
-          "top-0": alertMsg != "nil",
-        }
-      )}
-    >
-      <span className="text-center leading-13 w-fit px-5 h-13 bg-primary-bg border border-border-bg rounded-2xl shadow-[0px_0px_100px_0px_#000000] transition-all ease-in-out duration-300">
-        <p className="text-white font-github text-balance font-medium">
-          {props.message}
-        </p>
-      </span>
-    </div>
-  );
+	return (
+		<div
+			className={clsx(
+				"select-none w-full h-20 absolute z-50 mx-auto flex justify-center items-center transition-all ease-in-out duration-300",
+				{
+					"-top-50": alertMsg == "nil",
+					"top-0": alertMsg != "nil",
+				},
+			)}
+		>
+			<span className="text-center leading-13 w-fit px-5 h-13 bg-primary-bg border border-border-bg rounded-2xl shadow-[0px_0px_100px_0px_#000000] transition-all ease-in-out duration-300">
+				<p className="text-white font-github text-balance font-medium">
+					{props.message}
+				</p>
+			</span>
+		</div>
+	);
 };
 
 export default Alert;
--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,11 +1,15 @@
 const Error = () => {
-  return (
-    <div className="z-100 flex flex-col justify-center items-center w-screen h-screen absolute top-0 bg-white lg:hidden">
-        <p className="text-center text-black text-5xl font-github font-bold">Welp! 😭</p>
-        <br className="my-2"/>
-        <p className="text-center text-black text-3xl font-github font-bold">This site only works on a Laptop or Desktop</p>
-    </div>
-  )
-}
+	return (
+		<div className="z-100 flex flex-col justify-center items-center w-screen h-screen absolute top-0 bg-white lg:hidden">
+			<p className="text-center text-black text-5xl font-github font-bold">
+				Welp! 😭
+			</p>
+			<br className="my-2" />
+			<p className="text-center text-black text-3xl font-github font-bold">
+				This site only works on a Laptop or Desktop
+			</p>
+		</div>
+	);
+};
 
-export default Error
+export default Error;
