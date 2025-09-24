b51f825bb1c9ff0774b4a122b35c1105f1f3b51a
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-09-24 08:49:57 +0530

    Initial Attempt

---
+++ src/store.js
@@ -0,0 +1,7 @@
+// Contains all the global state variables/stores
+
+// Store for keeping track of Current Editor State
+let currentEditorState = undefined;
+
+// A reference to the Konva Layer Node
+let layerRef = undefined;
