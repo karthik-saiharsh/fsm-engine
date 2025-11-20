acf42f07e2dd24647fdb294ab1e8d096ad44e7b2
Revanth Singothu <revanthsingothu@gmail.com>
2025-11-20 09:51:44 +0000

    fix: UI quirks in guide component fixed

--- src/app.jsx
+++ src/app.jsx
@@ -1,57 +1,57 @@
-import Dock from "./components/Dock";
-import Editor from "./components/Editor";
-import Settings from "./components/Settings";
-import Controls from "./components/Controls";
 import { useEffect } from "react";
 import Alert from "./components/Alert";
-import Popup from "./components/Popup";
+import Controls from "./components/Controls";
+import Dock from "./components/Dock";
+import Editor from "./components/Editor";
 import Guide from "./components/Guide";
+import Popup from "./components/Popup";
 import SaveDialog from "./components/SaveDialog";
+import Settings from "./components/Settings";
 import { handleShortCuts } from "./lib/editor";
 
 export function App() {
-  // Disable right click context menu
-  // Got this useEffect code from StackOverflow
-  useEffect(() => {
-    const handleContextmenu = (e) => {
-      e.preventDefault();
-    };
-    document.addEventListener("contextmenu", handleContextmenu);
-    return function cleanup() {
-      document.removeEventListener("contextmenu", handleContextmenu);
-    };
-  }, []);
+	// Disable right click context menu
+	// Got this useEffect code from StackOverflow
+	useEffect(() => {
+		const handleContextmenu = (e) => {
+			e.preventDefault();
+		};
+		document.addEventListener("contextmenu", handleContextmenu);
+		return function cleanup() {
+			document.removeEventListener("contextmenu", handleContextmenu);
+		};
+	}, []);
 
-  // Add KeyBoard Shortcuts
-  function handleKeyPress(event) {
-    handleShortCuts(event.key);
-  }
+	// Add KeyBoard Shortcuts
+	function handleKeyPress(event) {
+		handleShortCuts(event.key);
+	}
 
-  useEffect(() => {
-    document.addEventListener("keyup", handleKeyPress);
+	useEffect(() => {
+		document.addEventListener("keyup", handleKeyPress);
 
-    return () => {
-      document.removeEventListener("keyup", handleKeyPress);
-    };
-  }, [handleKeyPress]);
+		return () => {
+			document.removeEventListener("keyup", handleKeyPress);
+		};
+	}, [handleKeyPress]);
 
-  return (
-    <div id="body" className="w-screen h-screen bg-primary-bg overflow-hidden">
-      <Editor />
+	return (
+		<div id="body" className="w-screen h-screen bg-primary-bg overflow-hidden">
+			<Editor />
 
-      <Dock />
+			<Dock />
 
-      <Settings />
+			<Settings />
 
-      <Controls />
+			<Controls />
 
-      <Alert />
+			<Alert />
 
-      <Popup />
+			<Popup />
 
-      <Guide />
+			<Guide />
 
-      <SaveDialog />
-    </div>
-  );
+			<SaveDialog />
+		</div>
+	);
 }
