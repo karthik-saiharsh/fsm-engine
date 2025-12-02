e2875d31591e85b801330438497668f304fdcdfb
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-12-02 19:39:16 +0530

    Minor UX Update
    - the transitions labels array is now sorted before a update in the UI
    - Added a function in special_functions.js that produces a minimized version of NodeList. This will be useful for the next steps in the roadmap

--- src/lib/special_functions.js
+++ src/lib/special_functions.js
@@ -1,60 +1,95 @@
+/*
+This file contains all the algorithmic implementations
+*/
+
 import { HandleAutoLayout } from "./editor";
 import {
-	deleted_nodes,
-	engine_mode,
-	node_list,
-	store,
-	transition_list,
+  deleted_nodes,
+  engine_mode,
+  node_list,
+  store,
+  transition_list,
 } from "./stores";
 
 export async function HandleLoadFSM() {
-	// Create a hidden file input
-	const input = document.createElement("input");
-	input.type = "file";
-	input.accept = ".fsm,application/json";
+  // Create a hidden file input
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".fsm,application/json";
 
-	// Handle file selection
-	input.onchange = async (event) => {
-		const file = event.target.files[0];
-		if (!file) return;
+  // Handle file selection
+  input.onchange = async (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
 
-		const text = await file.text();
-		try {
-			const data = JSON.parse(text); // assign to global variable
-			store.set(node_list, () => data.nodes);
-			store.set(transition_list, () => data.transitions);
-			store.set(deleted_nodes, () => data.deleted_nodes);
-			store.set(engine_mode, () => data.engine_mode);
-		} catch (err) {
-			console.error("Invalid JSON:", err);
-		}
+    const text = await file.text();
+    try {
+      const data = JSON.parse(text); // assign to global variable
+      store.set(node_list, () => data.nodes);
+      store.set(transition_list, () => data.transitions);
+      store.set(deleted_nodes, () => data.deleted_nodes);
+      store.set(engine_mode, () => data.engine_mode);
+    } catch (err) {
+      console.error("Invalid JSON:", err);
+    }
 
-		// Clean up
-		document.body.removeChild(input);
+    // Clean up
+    document.body.removeChild(input);
 
-		HandleAutoLayout(); // Format the FSM
-	};
+    HandleAutoLayout(); // Format the FSM
+  };
 
-	document.body.appendChild(input);
-	input.click();
+  document.body.appendChild(input);
+  input.click();
 }
 
 export function getTransitionDetails(transitions, id) {
-	let end_nodes = [];
-	// Create arrays for each of the alphabets
-	const alphabets = store.get(engine_mode).alphabets;
+  let end_nodes = [];
+  // Create arrays for each of the alphabets
+  const alphabets = store.get(engine_mode).alphabets;
 
-	alphabets.forEach((alpha) => {
-		const valid_trs = transitions.filter((tr) => {
-			const transitionObj = store.get(transition_list)[tr.tr_name];
-			return (
-				transitionObj && tr.from == id && transitionObj.name.includes(alpha)
-			);
-		});
-		const valid_states = valid_trs.map(
-			(tr) => store.get(node_list)[tr.to].name,
-		);
-		end_nodes.push(valid_states);
-	});
-	return end_nodes;
+  alphabets.forEach((alpha) => {
+    const valid_trs = transitions.filter((tr) => {
+      const transitionObj = store.get(transition_list)[tr.tr_name];
+      return (
+        transitionObj && tr.from == id && transitionObj.name.includes(alpha)
+      );
+    });
+    const valid_states = valid_trs.map(
+      (tr) => store.get(node_list)[tr.to].name
+    );
+    end_nodes.push(valid_states);
+  });
+  return end_nodes;
+}
+
+/************ HELPER FUNCTIONS *************/
+export function getGraph() {
+  // This functions returns an adjacency list representation of the Automata
+  const NodeList = store.get(node_list);
+  const TransitionList = store.get(transition_list);
+  const Graph = {}; // Graph DS
+
+  NodeList.forEach((node) => {
+    if (node) {
+      // Make sure node ain't null
+      const id = node.id;
+      const transitions = node.transitions.map((tr) =>
+        tr.from == id
+          ? {
+              to: tr.to,
+              id: tr.tr_name,
+              on: TransitionList[tr.tr_name].name,
+            }
+          : {}
+      );
+
+      Graph[id] = {
+        name: node.name,
+        transitions: transitions,
+      };
+    }
+  });
+
+  return Graph;
 }
--- src/lib/transitions.js
+++ src/lib/transitions.js
@@ -1,89 +1,89 @@
 import {
-	active_transition,
-	editor_state,
-	node_list,
-	show_popup,
-	stage_ref,
-	store,
-	transition_list,
+  active_transition,
+  editor_state,
+  node_list,
+  show_popup,
+  stage_ref,
+  store,
+  transition_list,
 } from "./stores";
 import { addToHistory } from "./history";
 
 // Handle a click event on a transition
 export function handleTransitionClick(id) {
-	if (store.get(editor_state) === "Remove") {
-		const from_state = store.get(transition_list)[id].from;
-		const to_state = store.get(transition_list)[id].to;
+  if (store.get(editor_state) === "Remove") {
+    const from_state = store.get(transition_list)[id].from;
+    const to_state = store.get(transition_list)[id].to;
 
-		// Delete the Display arrow
-		const transition = store.get(stage_ref).findOne(`#tr_${id}`);
-		transition.destroy();
+    // Delete the Display arrow
+    const transition = store.get(stage_ref).findOne(`#tr_${id}`);
+    transition.destroy();
 
-		// Remove this transition in store
-		store.set(transition_list, (old) => {
-			const newTrList = [...old];
-			newTrList[id] = undefined;
-			return newTrList;
-		});
+    // Remove this transition in store
+    store.set(transition_list, (old) => {
+      const newTrList = [...old];
+      newTrList[id] = undefined;
+      return newTrList;
+    });
 
-		// Remove this transition from Node
-		store.set(node_list, (old) => {
-			const newNodes = [...old];
+    // Remove this transition from Node
+    store.set(node_list, (old) => {
+      const newNodes = [...old];
 
-			newNodes[from_state] = {
-				...newNodes[from_state],
-				transitions: newNodes[from_state].transitions.filter(
-					(tr) => tr.from !== from_state || tr.to !== to_state,
-				),
-			};
+      newNodes[from_state] = {
+        ...newNodes[from_state],
+        transitions: newNodes[from_state].transitions.filter(
+          (tr) => tr.from !== from_state || tr.to !== to_state
+        ),
+      };
 
-			if (from_state !== to_state) {
-				newNodes[to_state] = {
-					...newNodes[to_state],
-					transitions: newNodes[to_state].transitions.filter(
-						(tr) => tr.from !== from_state || tr.to !== to_state,
-					),
-				};
-			}
-			return newNodes;
-		});
-		addToHistory();
-		return;
-	}
-	store.set(show_popup, true);
-	store.set(active_transition, () => id);
+      if (from_state !== to_state) {
+        newNodes[to_state] = {
+          ...newNodes[to_state],
+          transitions: newNodes[to_state].transitions.filter(
+            (tr) => tr.from !== from_state || tr.to !== to_state
+          ),
+        };
+      }
+      return newNodes;
+    });
+    addToHistory();
+    return;
+  }
+  store.set(show_popup, true);
+  store.set(active_transition, () => id);
 }
 
 // Handle Save on Changing a Transition's Label
 export function handleTransitionSave(labels) {
-	addToHistory();
-	// Update the New Labels in store
-	store.set(show_popup, false);
-	store.set(transition_list, (old) => {
-		const newTrList = [...old];
-		if (newTrList[store.get(active_transition)]) {
-			newTrList[store.get(active_transition)] = {
-				...newTrList[store.get(active_transition)],
-				name: labels,
-			};
-		}
-		return newTrList;
-	});
+  addToHistory();
+  // Update the New Labels in store
+  store.set(show_popup, false);
+  store.set(transition_list, (old) => {
+    const newTrList = [...old];
+    if (newTrList[store.get(active_transition)]) {
+      newTrList[store.get(active_transition)] = {
+        ...newTrList[store.get(active_transition)],
+        name: labels.toSorted(), // Sort them before updating labels
+      };
+    }
+    return newTrList;
+  });
 
-	// Update the new Labels in UI
-	const displayText = store
-		.get(stage_ref)
-		.findOne(`#trtext_${store.get(active_transition)}`);
-	displayText.text(labels.toString());
+  // Update the new Labels in UI
+  const displayText = store
+    .get(stage_ref)
+    .findOne(`#trtext_${store.get(active_transition)}`);
+  displayText.text(labels.toString());
 
-	// Update Position in UI
-	const label = store
-		.get(stage_ref)
-		.findOne(`#tr_label${store.get(active_transition)}`);
+  // Update Position in UI
+  const label = store
+    .get(stage_ref)
+    .findOne(`#tr_label${store.get(active_transition)}`);
 
-	const points =
-		store.get(transition_list)[store.get(active_transition)].points;
+  const points =
+    store.get(transition_list)[store.get(active_transition)].points;
 
-	label.x(points[2] - 2 * labels.toString().length);
-	store.set(active_transition, () => null);
+  label.x(points[2] - 2 * labels.toString().length);
+  store.set(active_transition, () => null);
 }
